fe714576c5203ab01889007145d440eb4ace229d
celoazevedo <meazevedo86@gmail.com>
2021-02-13 11:42:35 -0800

    complete highest rank number challenge

---
+++ 6Kyu/highestRankNum.js
@@ -0,0 +1,25 @@
+/*Complete the method which returns the number which is most frequent in the given input array. If there is a tie for most frequent number, return the largest number among them.
+
+Note: no empty arrays will be given.
+
+Examples
+[12, 10, 8, 12, 7, 6, 4, 10, 12]              -->  12
+[12, 10, 8, 12, 7, 6, 4, 10, 12, 10]          -->  12
+[12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3 */
+
+function highestRank(arr) {
+  let counterF = 1;
+  let counter = 0;
+  let item;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length; j++) {
+      if (arr[i] == arr[j]) counter++;
+      if (counterF < counter) {
+        counterF = counter;
+        item = arr[i];
+      }
+    }
+    counter = 0;
+  }
+  return item;
+}
