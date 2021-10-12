b6b1ecb329342f3e5f65e16a9407cfcddba1d55b
celoazevedo <meazevedo86@gmail.com>
2021-10-12 11:17:19 -0700

    complete challenge

---
+++ 7thKyu/countNumOfExcAndQuestReturnProd.js
@@ -0,0 +1,29 @@
+/**
+ * Description:
+Count the number of exclamation marks and question marks, return the product.
+
+Examples
+Product("") == 0
+product("!") == 0
+Product("!ab? ?") == 2
+Product("!!") == 0
+Product("!??") == 2
+Product("!???") == 3
+Product("!!!??") == 6
+Product("!!!???") == 9
+Product("!???!!") == 9
+Product("!????!!!?") == 20
+ */
+
+function product(s){
+    let counterExc = 0;
+    let counterQuest = 0;
+    for (let i = 0; i < s.length; i++) {
+      if (s.split('')[i] === '!') {
+        counterExc++
+      } else if (s.split('')[i] === '?') {
+        counterQuest++
+      }
+    }
+    return counterExc * counterQuest
+  }
